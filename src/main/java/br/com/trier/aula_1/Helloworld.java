efe140809df8973550d028caa95c2daaa0246bd2
martinslp0 <marianapinhompdo@gmail.com>
2023-05-22 15:51:17 -0300

    Segundo commit

--- src/main/java/br/com/trier/aula_1/Helloworld.java
+++ src/main/java/br/com/trier/aula_1/Helloworld.java
@@ -4,7 +4,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hi");
+		System.out.println("Olá Mund!");
+		System.out.println("**********");
 	}
 
 }
